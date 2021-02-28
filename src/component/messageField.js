85a42ea2404015d3bc3b25078ea6b59c6e8a92c4
Margarita <mpankratova13@gmail.com>
2021-02-28 21:24:45 +0300

    Made Task3

---
+++ src/component/messageField.js
@@ -0,0 +1,71 @@
+import { useState, useCallback, useEffect, useRef } from "react";
+import MessageItem from './messageItem';
+import { ARR, BOT_NAME, BOT_TEXT } from '../const';
+import RedoIcon from '@material-ui/icons/Redo';
+
+export default function MessageField() {
+    const [messageArr, setMessageArr] = useState(ARR);
+    const [inValue, setInValue] = useState('');
+    const messFieldEl = useRef();
+    const messInputEl = useRef();
+
+    const addMessage = useCallback((event) => {
+        event.preventDefault();
+        if (inValue) {
+            setMessageArr([...messageArr, { text: inValue, author: 'Аноним' }]);
+            messInputEl.current.className = 'norm';
+            setInValue('');
+        } else {
+            messInputEl.current.className = 'error';
+        }
+        messInputEl.current.focus();
+    }, [inValue]);
+
+    useEffect(() => {
+        messInputEl?.current.focus();
+    }, []);
+
+    useEffect(() => {
+        //bot answer
+        let timerID = null;
+        const lastAuthor = messageArr[messageArr.length - 1].author;
+        if (lastAuthor && lastAuthor !== BOT_NAME) {
+            timerID = setTimeout(() => {
+                setMessageArr([...messageArr, { text: lastAuthor + BOT_TEXT, author: BOT_NAME }]);
+            }, 1000)
+        }
+
+        //scroll
+        if (messFieldEl) {
+            messFieldEl.current.scrollTop = messFieldEl.current.scrollHeight;
+        }
+
+        //willUnmount
+        return () => {
+            clearTimeout(timerID);
+        }
+    }, [messageArr]);
+
+
+    const renderMessage = useCallback((message) => {
+        return (<MessageItem text={message.text} author={message.author} />);
+    }, []);
+
+    const changeInValue = useCallback((event) => {
+        setInValue(event.target.value);
+    }, []);
+
+    return (
+        <div className="app__field">
+            <div ref={messFieldEl} className="message__field">
+                {messageArr.map(renderMessage)}
+            </div>
+            <form className="sendMess__form" onSubmit={addMessage}>
+                <input ref={messInputEl} type="text" name="message__input" placeholder="Your message"
+                    value={inValue} onChange={changeInValue}>
+                </input>
+                <button type="submit"><RedoIcon /></button>
+            </form>
+        </div >
+    );
+}
