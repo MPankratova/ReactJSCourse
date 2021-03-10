013916c86ad6dbb0230023355d3f7eb65a8d4a1f
Margarita <mpankratova13@gmail.com>
2021-03-10 14:00:57 +0300

    Made Task6

---
+++ src/const.js
@@ -0,0 +1,8 @@
+export const BOT_NAME = 'bot';
+export const BOT_TEXT = ', привет, чем я могу помочь?';
+
+export const CHAT_TYPE = [
+    'group chat',
+    'personal chat',
+    'private chat'
+];
